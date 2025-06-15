7f6d9e3c0bf3b700c4a689d5c005f20df14b1d7b
Knee <lukeedo@icloud.com>
2025-06-15 16:48:12 -0400

    refactor: remove server readiness logic and update Bar component to load local HTML file

--- app.ts
+++ app.ts
@@ -2,18 +2,6 @@ import { App, Gdk, Gtk } from "astal/gtk3"
 import { subprocess } from "astal/process"
 import Bar from "./elements/Bar"
 
-let serverReady = false
-let serverReadyPromise: Promise<void>
-let resolveServerReady: (() => void)
-
-serverReadyPromise = new Promise((resolve) => {
-    resolveServerReady = resolve
-})
-
-export function waitUntilServerReady(): Promise<void> {
-    return serverReady ? Promise.resolve() : serverReadyPromise
-}
-
 function main() {
     const bars = new Map<Gdk.Monitor, Gtk.Widget>()
 
@@ -30,15 +18,5 @@ function main() {
         bars.delete(gdkmonitor)
     })
 }
-
-subprocess(
-    ["npm", "--prefix", "~/.config/ags/svelte-web", "run", "start"],
-    (stdout) => {
-        if (stdout.includes("➜  Local:   http://localhost:8125/")) {
-            serverReady = true
-            resolveServerReady()
-        }
-    }
-)
 
 App.start({ main })
--- elements/Bar.tsx
+++ elements/Bar.tsx
@@ -1,7 +1,6 @@
 import { Astal, Gdk } from "astal/gtk3"
 import WebKit2 from "gi://WebKit2?version=4.1"
 import GtkLayerShell from "gi://GtkLayerShell"
-import { waitUntilServerReady } from "../app"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
     const webview = WebKit2.WebView.new()
@@ -9,6 +8,10 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
     webview.set_background_color(new Gdk.RGBA())
     webview.set_can_focus(true)
     webview.set_sensitive(true)
+    
+    const settings = webview.get_settings();
+    settings.allow_file_access_from_file_urls = true;
+
 
     return <window
         gdkmonitor={gdkmonitor}
@@ -23,9 +26,8 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
             self.set_accept_focus(true)
             self.add(webview)
             webview.show_all()
-            
-            await waitUntilServerReady()
-            webview.load_uri("http://localhost:8125")
+
+            webview.load_uri("file:///home/knee/.config/ags/svelte-web/dist/index.html")
         }}
     />
 }
